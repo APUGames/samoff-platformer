6479c0f33705f88f83b4d53f8380e9a8f46c5bfc
Tim <tsamoff@apu.edu>
2023-01-31 15:52:44 -0800

    Implemented jump and climb

--- Platformer/Assets/scripts/Player.cs
+++ Platformer/Assets/scripts/Player.cs
@@ -6,15 +6,28 @@ public class Player : MonoBehaviour
 {
     Rigidbody2D playerCharacter;
     Animator playerAnimator;
+    Collider2D playerCollider;
 
     [Tooltip("Change this value to change the run speed.")]
     [SerializeField] float runSpeed = 5.0f;
+
+    [Tooltip("Change this value to change the jump speed.")]
+    [SerializeField] float jumpSpeed = 5.0f;
+
+    [Tooltip("Change this value to change the climb speed.")]
+    [SerializeField] float climbSpeed = 5.0f;
+
+    private float gravityScaleAtStart;
 
     // Start is called before the first frame update
     void Start()
     {
         playerCharacter = GetComponent<Rigidbody2D>();
         playerAnimator = GetComponent<Animator>();
+        playerCollider = GetComponent<Collider2D>();
+
+        // Store Gravity Scale when game starts
+        gravityScaleAtStart = playerCharacter.gravityScale;
     }
 
     // Update is called once per frame
@@ -22,6 +35,8 @@ public class Player : MonoBehaviour
     {
         Run();
         FlipSprite();
+        Jump();
+        Climb();
     }
 
     private void Run()
@@ -30,6 +45,8 @@ public class Player : MonoBehaviour
         float hMovement = Input.GetAxis("Horizontal");
         Vector2 runVelocity = new Vector2(hMovement * runSpeed, playerCharacter.velocity.y);
         playerCharacter.velocity = runVelocity;
+
+        print(runVelocity);
 
         bool hSpeed = Mathf.Abs(playerCharacter.velocity.x) > Mathf.Epsilon;
 
@@ -46,6 +63,47 @@ public class Player : MonoBehaviour
             // Reverse the current scaling of the X-axis to flip the sprite
             transform.localScale = new Vector2(Mathf.Sign(playerCharacter.velocity.x), 1f);
         }
+    }
+
+    private void Jump()
+    {
+        // Will stop the function unless true
+        if(!playerCollider.IsTouchingLayers(LayerMask.GetMask("Ground")))
+        {
+            return;
+        }
+
+        if(Input.GetButtonDown("Jump"))
+        {
+            // Get new Y velocity based on a controllable variable
+            Vector2 jumpVelocity = new Vector2(0.0f, jumpSpeed);
+            playerCharacter.velocity += jumpVelocity;
+        }
+    }
+
+    private void Climb()
+    {
+        // Will stop the function unless true
+        if (!playerCollider.IsTouchingLayers(LayerMask.GetMask("Climbing")))
+        {
+            playerAnimator.SetBool("climb", false);
+            playerCharacter.gravityScale = gravityScaleAtStart;
+            return;
+        }
+
+        // "Vertical" from Input Manager
+        float vMovement = Input.GetAxis("Vertical");
+
+        // X needs to remain the same and we need to change Y
+        Vector2 climbVelocity = new Vector2(playerCharacter.velocity.x, vMovement * climbSpeed);
+
+        playerCharacter.velocity = climbVelocity;
+
+        playerCharacter.gravityScale = 0.0f;
+
+        bool vSpeed = Mathf.Abs(playerCharacter.velocity.y) > Mathf.Epsilon;
+
+        playerAnimator.SetBool("climb", vSpeed);
     }
 
 }
